a309b1209b2a128abab6b74157f9d71550941f95
Ce-bit123 <118348146+Ce-bit123@users.noreply.github.com>
2023-08-21 23:31:24 +0800

    Update appSSSI.py

--- appSSSI.py
+++ appSSSI.py
@@ -13,13 +13,13 @@ st.header("Risk prediction of early neurological deterioration within 72 hours i
 
 # Input bar 1
 # Input bar 2
-NIHSS = st.number_input("Enter NIHSS score on admission")
+NIHSSscore = st.number_input("Enter NIHSS score on admission")
 #NIHSS_score_after_thrombolysis1 = st.number_input("Enter NIHSS score after thrombolysis")
 hemoglobin = st.number_input("Enter Hemoglobin(g/L)")
 #age = st.number_input("Enter Age(years)")
 #fasting_blood_glucose= st.number_input("Enter Fasting blood glucose")
 # Dropdown input
-posterior = st.selectbox("Whether it conforms to the posterior type", ("Yes", "No"))
+posteriortype = st.selectbox("Whether it conforms to the posterior type", ("Yes", "No"))
 antiplatelet = st.selectbox("Whether dual antiplatelet therapy was administered after admission",("Yes","No"))
 PAD = st.selectbox("Whether it manifests as parent artery disease",("Yes","No"))
 # If button is pressed
@@ -28,7 +28,7 @@ if st.button("Submit"):
     # Unpickle classifier
     clf = joblib.load("clfSSSIENDxgboost.pkl")
     # Store inputs into dataframe
-    X = pd.DataFrame([[antiplatelet,hemoglobin,NIHSS,posterior,PAD]],
+    X = pd.DataFrame([[antiplatelet,hemoglobin,NIHSSscore,posteriortype,PAD]],
                      columns=["antiplatelet", "hemoglobin","NIHSSscore",
                        "posteriortype","PAD"])
     X = X.replace(["Yes", "No"], [1, 0])
